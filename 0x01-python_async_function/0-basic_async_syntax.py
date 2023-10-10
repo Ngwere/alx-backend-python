5233e5db544eadc02660c13e07d43e3c8cdaeb7e
Ngwere <ngwerecliford@gmail.com>
2023-10-10 17:01:49 +0000

    created readme and task one

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -12,6 +12,7 @@ async def wait_random(max_delay: int =10) -> float:
         floating value random time
 
     """
+
     delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     
