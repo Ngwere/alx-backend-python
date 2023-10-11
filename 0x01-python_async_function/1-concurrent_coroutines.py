6154d59db64d4a93ce0df2d8a506e85908bff62c
Ngwere <ngwerecliford@gmail.com>
2023-10-11 03:14:56 +0000

    corrected tasks 1 and 2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -13,15 +13,15 @@
 import asyncio
 import random
 from typing import List
-task_wait_random = __import__('3-tasks').task_wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """ Waits for ran delay until max_delay, returns list of actual delays """
     spawn_ls = []
     delay_ls = []
     for i in range(n):
-        delayed_task = asyncio.create_task(task_wait_random(max_delay))
+        delayed_task = asyncio.create_task(wait_random(max_delay))
         delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
         spawn_ls.append(delayed_task)
 
